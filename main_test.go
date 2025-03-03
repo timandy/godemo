f9062f0b1e4e69c8926e05e08a58b7c689245e9c
TimAndy <xuchonglei@126.com>
2025-03-03 16:42:14 +0800

    测试

--- main_test.go
+++ main_test.go
@@ -5,28 +5,51 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/timandy/routinex/tools/consts"
 	"github.com/timandy/routinex/tools/exec"
+	"github.com/timandy/routinex/tools/file"
 )
 
 var goToolDir = exec.RunCmdOutput([]string{"go", "env", "GOTOOLDIR"})
 
 func TestHelp(t *testing.T) {
+	tracker := file.NewFileTracker(&os.Stderr)
+	tracker.Begin()
+	defer tracker.End()
+	//
 	args := []string{"routinex", "-h", "/demo", "-p", "ttt", "go", "version"}
 	os.Args = args
 	main()
+	//
+	output := tracker.Value()
+	assert.Contains(t, output, "Usage: routinex")
 }
 
 func TestOtherCmd(t *testing.T) {
+	tracker := file.NewFileTracker(&os.Stdout)
+	tracker.Begin()
+	defer tracker.End()
+	//
 	args := []string{"routinex", "-p", "/demo", "-p", "ttt", "git", "version"}
 	os.Args = args
 	main()
+	//
+	output := tracker.Value()
+	assert.Contains(t, output, "git version")
 }
 
 func TestOtherCmdHelp(t *testing.T) {
+	tracker := file.NewFileTracker(&os.Stdout)
+	tracker.Begin()
+	defer tracker.End()
+	//
 	args := []string{"routinex", "-p", "/demo", "-p", "ttt", "git", "-h"}
 	os.Args = args
 	main()
+	//
+	output := tracker.Value()
+	assert.Contains(t, output, "usage: git")
 }
 
 func TestCompileCmdHelp(t *testing.T) {
